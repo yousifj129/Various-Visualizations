cf1a2838e262432f96be65cb51e66fc149df3b2a
sebe324 <sebeq05@gmail.com>
2024-03-17 15:08:06 +0100

    Added utils file for some utility functions

---
+++ utils.cpp
@@ -0,0 +1,9 @@
+#include "utils.h"
+
+int Utils::randomInt(int min, int max)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(min,max);
+    return dis(gen);
+}
---
+++ utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <random>
+
+
+namespace Utils
+{
+int randomInt(int min, int max);
+}
+#endif // UTILS_H
